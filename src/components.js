a253cee954a503ed2b19b1079605b34bc2393d90
Cecco <ceccodrummer@gmail.com>
2013-07-12 19:10:01 +0200

    Aggiunti nuovi sprite

--- src/components.js
+++ src/components.js
@@ -18,20 +18,21 @@ Crafty.c('KenCharacter', {
     init: function() {
         this.requires('PlayerCharacter, SpriteAnimation, ken_punch')
             .onHit('Solid', this.stopMovement)
-            .animate('PlayerIdle', [[0,0], [0,1], [1,0]])
+            .animate('PlayerIdle', [[0,0], [1,0], [2,0], [3,0]])
             .animate('PlayerIdle', 45, -1)
-            .flip('X');
     }
 });
 
 Crafty.c('RyuCharacter', {
     init: function() {
-        this.requires('PlayerCharacter, SpriteAnimation, ryu_punch, Twoway')
+        this.requires('PlayerCharacter, SpriteAnimation, ryu_walking, Twoway')
             .gravity("Platform")
             .onHit('Solid', this.stopMovement)
-            .animate('PlayerIdle', 0, 0, 2)
+            //.animate('PlayerIdle', [[10,0], [11,0], [12,0],[13,0], [14,0], [15,0], [16,0]]) 
+            .animate('PlayerIdle', 4, 0, 8)
             .animate('PlayerIdle', 40, -1)
-            .twoway(4, 6);
+            .twoway(4, 6)
+            .flip('X');;
     }
 });
 
--- src/scenes.js
+++ src/scenes.js
@@ -1,52 +1,59 @@
-/* 
- * SCENE
- */
+/*
+* SCENE
+*/
 
 // Loading scene
 // -------------
 // Handles the loading of binary assets such as images and audio files
-Crafty.scene('Loading', function(){
-  // Draw some text for the player to see in case the file
-  //  takes a noticeable amount of time to load
-  Crafty.e('2D, DOM, Text')
-    .text('Loading...')
-    .attr({ x: 0, y: Game.height()/2 - 24, w: Game.width() });
+Crafty.scene('Loading', function() {
+	// Draw some text for the player to see in case the file
+	//  takes a noticeable amount of time to load
+	Crafty.e('2D, DOM, Text').text('Loading...').attr({
+		x : 0,
+		y : Game.height() / 2 - 24,
+		w : Game.width()
+	});
 
-  // Load our sprite map image
-  Crafty.load(['assets/Ken.png', 
-      'assets/Ryu.png',
-      'assets/bg.gif'], function(){
-      
-        // Inizializzazione sprite
-        
-        Crafty.sprite(90, 120, 'assets/Ken.png', {
-          ken_punch:  [0, 0]
-        });
-        
-        Crafty.sprite(100, 100, 'assets/Ryu_x3.png', {
-          ryu_punch:  [0, 0]
-        }); 
-        
-        Crafty.scene('Game');
-      });
+	// Load our sprite map image
+	Crafty.load(['assets/ken_sprite.gif', 'assets/ryu_sprite.gif', 'assets/bg.gif'], function() {
+
+		// Inizializzazione sprite
+
+		Crafty.sprite(50, 105, 'assets/ken_sprite.gif', {
+			ken_punch : [0, 0]
+		});
+
+		Crafty.sprite(50, 105, 'assets/ryu_sprite.gif', {
+			ryu_idle : [0, 0],
+			ryu_jump : [9, 0]
+		});
+		
+		Crafty.sprite(49, 105, 'assets/ryu_sprite.gif', {
+			ryu_walking : [4, 0]
+		});
+
+		Crafty.scene('Game');
+	});
 });
 
+Crafty.scene('Game', function() {
+	Crafty.background('url(assets/bg.gif)');
+	//Crafty.viewport.scroll('_x', 500);
 
-Crafty.scene('Game', function(){
-    Crafty.background('url(assets/bg.gif)');
-    //Crafty.viewport.scroll('_x', 500);
-    
-    Crafty.e("Platform, 2D, DOM")
-            .attr({ x: 0, y: 220, w: 400, h: 10 });
-    
+	Crafty.e("Platform, 2D, DOM").attr({
+		x : 0,
+		y : 220,
+		w : 400,
+		h : 10
+	});
 
-    var ryu = Crafty.e('RyuCharacter').attr({
-        x: 250,
-        y: 110
-    });
-    var ken = Crafty.e('KenCharacter').attr({
-        x: 50,
-        y: 110
-    });
-    
+	var ryu = Crafty.e('RyuCharacter').attr({
+		x : 250,
+		y : 110
+	});
+	var ken = Crafty.e('KenCharacter').attr({
+		x : 50,
+		y : 110
+	});
+
 });
